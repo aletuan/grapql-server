0b7099d79909a968d693a9ce808d3f2ad3230213
Tuan Anh Le <anhlt1983@gmail.com>
2018-06-15 21:09:04 +1000

    setup graphql schema

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 // console.log({starting: true});
 
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQuery',
+    description: 'The root query',
+    fields: {
+        user: {
+            type: GraphQLString,
+            resolve() {
+                return 'viewer!';
+            }
+        }
+    }
+});
+
+const Schema = new GraphQLSchema({
+    query: RootQuery
+});
+
 
 const app = express();
 
-app.use('/graphql', (req, res) => {
-    res.send({data: true});
-});
+app.use('/graphql', graphqlHTTP({schema: Schema}));
 
 app.listen(3000, () => {
     console.log({running: true});
